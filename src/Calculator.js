f045a70b17fbde3d372ba60fd7236d541487e23d
kry008 <pkrzysaaaa@gmail.com>
2022-02-02 10:10:41 +0100

    Przygotowanie wyglądu 2

--- src/Calculator.js
+++ src/Calculator.js
@@ -11,28 +11,28 @@ export default class Kalkulator extends React.Component {
         return(
             <div id="calc">
                 <div id="mw">
-
+                    1234567890/*-+
                 </div>
                 <div id="dw">
                     1234567890/*-+
                 </div>
                 <div id="panel">
-                    <div className="button">7</div>
-                    <div className="button">8</div>
-                    <div className="button">9</div>
-                    <div className="button">/</div>
-                    <div className="button">4</div>
-                    <div className="button">5</div>
-                    <div className="button">6</div>
-                    <div className="button">*</div>
-                    <div className="button">1</div>
-                    <div className="button">2</div>
-                    <div className="button">3</div>
-                    <div className="button">-</div>
-                    <div className="button">0</div>
-                    <div className="button">«</div>
-                    <div className="button">=</div>
-                    <div className="button">+</div>
+                    <div className="button" id="7">7</div>
+                    <div className="button" id="8">8</div>
+                    <div className="button" id="9">9</div>
+                    <div className="button" id="/">/</div>
+                    <div className="button" id="4">4</div>
+                    <div className="button" id="5">5</div>
+                    <div className="button" id="6">6</div>
+                    <div className="button" id="*">*</div>
+                    <div className="button" id="1">1</div>
+                    <div className="button" id="2">2</div>
+                    <div className="button" id="3">3</div>
+                    <div className="button" id="-">-</div>
+                    <div className="button" id="0">0</div>
+                    <div className="button" id="«">«</div>
+                    <div className="button" id="=">=</div>
+                    <div className="button" id="+">+</div>
                 </div>
             </div>
         )
